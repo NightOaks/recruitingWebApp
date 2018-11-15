9aad1cc78e8daac97247dfb4ca054c5e6263511c
Devin Simoneaux <devin.simoneaux@myemail.indwes.edu>
2018-11-14 22:36:16 -0500

    changed session file
    session update

--- Recruiting/session.php
+++ Recruiting/session.php
@@ -4,11 +4,14 @@
 	
 	$user_check = $_SESSION['login_user'];
 	
-	$ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+	// $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+	$sql = "SELECT fname FROM user where username = '$user_check'";
+	$result = $db->query($sql);
 	
-	$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+	// $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+	$row = $result->fetch_assoc();
 	
-	$login_session = $row['username'];
+	$login_session = $row['fname'];
 	
 	if(!isset($_SESSION['login_user'])){
 		header("location:login.php");
