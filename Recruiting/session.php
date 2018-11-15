c92601412ef3ecd51f63ec6631c5cbf77aa1e56c
Devin Simoneaux <devin.simoneaux@myemail.indwes.edu>
2018-11-14 22:38:43 -0500

    a
    a

--- Recruiting/session.php
+++ Recruiting/session.php
@@ -5,13 +5,13 @@
 	$user_check = $_SESSION['login_user'];
 	
 	// $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
-	$sql = "SELECT fname FROM user where username = '$user_check'";
+	$sql = "SELECT first FROM user where username = '$user_check'";
 	$result = $db->query($sql);
 	
 	// $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 	$row = $result->fetch_assoc();
 	
-	$login_session = $row['fname'];
+	$login_session = $row['first'];
 	
 	if(!isset($_SESSION['login_user'])){
 		header("location:login.php");
