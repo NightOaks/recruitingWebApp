e4e127d2a7047b7b68ee6bd24278c01474ba5efc
gbrown9 <gabe.brown@myemail.indwes.edu>
2018-11-29 08:38:45 -0500

    more ajax
    more stuff

--- Recruiting/recruit/request.php
+++ Recruiting/recruit/request.php
@@ -3,18 +3,14 @@
 include("../config.php");
 
 $q = $_GET['q'];
-/*
+
 $sql="SELECT fname, lname FROM player WHERE fname LIKE '%$q%' or lname LIKE '%$q%';;
 
 $array[] = $db->query($sql);
-*/
-$array = array("Nicole Ignasiak", "Katie Key");
 
-$list = array();
-
-foreach ($array as $destination) {
-    if (stripos($destination, $q) !== false){
-        $list[] = $destination;
+foreach ($array as $results){
+    if (stripos($results, $q) !== false){
+        $list[] = $results;
     }
 }
 
