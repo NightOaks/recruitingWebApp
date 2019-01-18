1bc8ef5868c65e43b2a12957bd45f6a7b92dfc63
nightoaks <devin.simoneaux@myemail.indwes.edu>
2019-01-18 11:01:52 -0500

    changed title on login page

--- Recruiting/login.php
+++ Recruiting/login.php
@@ -34,7 +34,7 @@
 <!DOCTYPE html>
 	
 	<head>
-		<title>Login Page</title>
+		<title>Recruiting Login<title>
     	<link rel="stylesheet" type="text/css" href="loginStyle.css">
     	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
