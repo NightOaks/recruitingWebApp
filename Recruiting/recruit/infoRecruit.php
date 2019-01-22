7bb8b8a4702d502c5ef56cda1ca3a586b44dc19c
gbrown9 <gabe.brown@myemail.indwes.edu>
2019-01-21 20:48:47 -0500

    update
    stuff

--- Recruiting/recruit/infoRecruit.php
+++ Recruiting/recruit/infoRecruit.php
@@ -15,15 +15,17 @@
     <title>IWU Recruiting</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="myStyle.css">
+    <link rel="stylesheet" type="text/css" href="../myStyle.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
   </head>
 
   <body>
     <?php
-      echo '<img class="playerProfileImage" src="data:image/jpeg;base64,'.base64_encode($player['profileImage']).'"/>
+    if ($player['profileImage'] !=NULL){
+      echo '<img class="playerProfileImage" src="data:image/jpeg;base64,'.base64_encode($player['profileImage']).'"/>';
+    }
 
-      <h2 style="text-align: center;">'.$player['fname'].' '.$player['lname'].'</h2>
+      echo '<h2 style="text-align: center;">'.$player['fname'].' '.$player['lname'].'</h2>
       <p style="text-align: center;">'.$player['year'].'</p>
       <p style="text-align: center;">'.$player['hs'].'</p>
       <p style="text-align: center;">'.$player['aau'].'</p>';
