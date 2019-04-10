fb07f4b821b2f531d148d01931e5425ce932443f
Nicole Ignasiak <nicole.ignasiak@myemail.indwes.edu>
2019-04-10 00:06:00 -0400

    evals display

--- Recruiting/recruit/infoRecruit.php
+++ Recruiting/recruit/infoRecruit.php
@@ -1,33 +1,28 @@
-
 <?php
+    /*-- we included connection files--*/
+      include "../config.php";
+      $sql = "SELECT * FROM player WHERE p_id = '$_GET[p_id]'";
+      $sql1 = "SELECT high_school.name FROM player INNER JOIN high_school ON player.hs_id=high_school.hs_id WHERE p_id = '$_GET[p_id]'";
+      $sql2 = "SELECT aau.name FROM player INNER JOIN aau ON player.aau_id=aau.aau_id WHERE p_id = '$_GET[p_id]'";
+      $result = $db->query($sql);
+      $result1 = $db->query($sql1);
+      $result2 = $db->query($sql2);
+      $player = $result->fetch_assoc();
+      $hs = $result1->fetch_assoc();
+      $aau = $result2->fetch_assoc();
+    ?>
 
-/*-- we included connection files--*/
-  include "../config.php";
-  $sql = "SELECT * FROM player WHERE p_id = '$_GET[p_id]'";
-  $sql1 = "SELECT high_school.name FROM player INNER JOIN high_school ON player.hs_id=high_school.hs_id WHERE p_id = '$_GET[p_id]'";
-  $sql2 = "SELECT aau.name FROM player INNER JOIN aau ON player.aau_id=aau.aau_id WHERE p_id = '$_GET[p_id]'";
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <title>IWU Recruiting</title>
 
-  $result = $db->query($sql);
-  $result1 = $db->query($sql1);
-  $result2 = $db->query($sql2);
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="recruit.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
 
-  $player = $result->fetch_assoc();
-  $hs = $result1->fetch_assoc();
-  $aau = $result2->fetch_assoc();
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>IWU Recruiting</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="recruit.css">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-  
-  <script>
+        <script>
     function DisplayStats() {
       var DisplayStats = document.getElementById("displayStat");
       var juice = document.getElementById("displayJuiceScores");
@@ -81,80 +76,6 @@
 
   </script>
 
-  </head>
-
-  <body>
-    <p><a href="recruitHome.php">Back</a></p>
-    <?php
-    /*-- we included connection files--*/
-      include "../config.php";
-      $sql = "SELECT * FROM player WHERE p_id = '$_GET[p_id]'";
-      $sql1 = "SELECT high_school.name FROM player INNER JOIN high_school ON player.hs_id=high_school.hs_id WHERE p_id = '$_GET[p_id]'";
-      $sql2 = "SELECT aau.name FROM player INNER JOIN aau ON player.aau_id=aau.aau_id WHERE p_id = '$_GET[p_id]'";
-      $result = $db->query($sql);
-      $result1 = $db->query($sql1);
-      $result2 = $db->query($sql2);
-      $player = $result->fetch_assoc();
-      $hs = $result1->fetch_assoc();
-      $aau = $result2->fetch_assoc();
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <title>IWU Recruiting</title>
-
-      echo "<div class='black-border'>
-
-      <button onclick='DisplayStats()'>Stats</button>
-      <button onclick='DisplayJuice()'>Juice</button>
-      <button onclick='DisplayEvals()'>Evaluations</button>
-
-      <form action='addEval.php' method='get'>
-        <input type='hidden' name='p_id' value=".$player['p_id'].">
-        <input class = 'btn text-color' type='submit' value='Evaluations'>
-      </form>
-      <hr>
-
-      <div class='hidden' id='displayStat'>
-        This is my Stat element.
-      </div>
-
-      <div id='displayJuiceScores' class='hidden'>
-        This is my Juice element.
-      </div>
-
-      
-
-      <div id='displayEval'>
-        This is my eval element.
-      </div>";
-
-      $sql3 = "SELECT * FROM evaluation WHERE p_id = '$_GET[p_id]'";
-      $evalList = $db->query($sql3);
-      $evalForm = '';
-
-      while ($evaluation = $evalList->fetch_assoc()){
-          $evalForm .= 
-            "<div class='padding'>
-                <input class='btn-recruit' value=".$evaluation['comment'].">
-            </div>";
-        }
-
-      echo $evalForm;
-
-      echo"</div>";
-
-      //<form action='addEval.php' method='post'>
-        //<input type='hidden' name='p_id' value=".$player['p_id']."/>
-        //<input class = 'btn text-color' type='submit' value='Done'>
-      //</form>
-
-    ?>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="recruit.css">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
       </head>
 
       <body>
@@ -184,13 +105,36 @@
           <p>'.$player['year'].'</p>
           <p>'.$hs['name'].'</p>
           <p>'.$aau['name'].'</p></div>';
+
           echo "<div class='black-border'>
+          <button onclick='DisplayStats()' class = 'btn text-color'>Stats</button>
+      <button onclick='DisplayJuice()' class = 'btn text-color'>Juice</button>
+      <button onclick='DisplayEvals()' class = 'btn text-color-eval'>Evaluations</button>
+
+          <hr>
+
+          <div id='displayEval'>
           <form action='addEval.php' method='get'>
             <input type='hidden' name='p_id' value=".$player['p_id'].">
-            <input class = 'btn text-color' type='submit' value='Evaluations'>
+            <input class = 'btn text-color' type='submit' value='Add an evaluation'>
           </form>
-          <hr>
-          </div>";
+          </div>
+          ";
+
+          $sql3 = "SELECT * FROM evaluation WHERE p_id = '$_GET[p_id]'";
+      $evalList = $db->query($sql3);
+      $evalForm = '';
+
+      while ($evaluation = $evalList->fetch_assoc()){
+          $evalForm .= 
+            "<div class='padding'>
+                <input class='btn-recruit' type='submit' value=".$evaluation['comment'].">
+            </div>";
+        }
+
+      echo $evalForm;
+
+          echo"</div>";
         ?>
     
     <div class="padding">
